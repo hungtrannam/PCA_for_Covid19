445012f147c4724bc04fc2b3390c5ee350a6cf02
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-08-04 13:53:10 +0700

    Update and rename multistats_markdown.R to multistats_markdown_covid_case.R

--- Modules/multistats_markdown_covid_case.R
+++ Modules/multistats_markdown_covid_case.R
@@ -12,44 +12,31 @@ library(igraph)
 # Truy cập đường dẫn dữ liệu
 setwd("D:/PCA_for_Covid19-main/Data")
 
-covid_cul <- read.csv("covid_case.csv",
+covid_case <- read.csv("covid_case.csv",
                       header = TRUE,
                       sep = ",",
                       stringsAsFactors = FALSE)
-covid_case <- read.csv("covid_cul.csv",
-                       header = TRUE,
-                       stringsAsFactors = FALSE)
 
 # Thiết lập định dạng
 covid_case$Day <- as.Date(covid_case$Day, format = "%d/%m/%Y")
-covid_cul$Day <- as.Date(covid_cul$Day, format = "%d/%m/%Y")
 
 # Trích lọc dữ liệu
 case_data <- covid_case %>% select(-Day)
-cul_data <- covid_cul %>% select(-Day)
 
 # Số chiều dữ liệu
 covid_case %>% dim()
-covid_cul %>% dim()
 
 # Định danh các biến
 case_data %>% names()
-cul_data %>% names()
 
 # Thống kê mô tả
 covid_case %>% sample_n(., 6)
-covid_cul %>% tail()
+covid_case %>% tail()
+covid_case %>% View()
 
 
 # Tương quan đồ
 corrplot::corrplot.mixed(cor_data <- case_data %>% cor(),
-                         tl.cex = 0.5,
-                         tl.col = "black",
-                         order = "hclust",
-                         addCoefasPercent = TRUE,
-                         upper = "ellipse")
-
-corrplot::corrplot.mixed(cor_data <- cul_data %>% cor(),
                          tl.cex = 0.5,
                          tl.col = "black",
                          order = "hclust",
@@ -65,27 +52,15 @@ Graph_pcor <- case_data %>% cor() %>%
                     sampleSize = nrow(case_data),
                     alpha = 0.05)
 
-Graph_pcor <- cul_data %>% cor() %>% 
-     qgraph::qgraph(.,
-                    graph = "pcor",
-                    layout = "spring",
-                    threshold = "bonferroni",
-                    sampleSize = nrow(cul_data),
-                    alpha = 0.05)
-
 # Sơ đồ sàng lọc (scree plot)
 case_data %>% psych::fa.parallel(., main = "Scree plot with parallel analysis")
-cul_data %>% psych::fa.parallel(., main = "Scree plot with parallel analysis")
 
 # Phân tích thành phần chính
 prcomp(case_data, center = TRUE, scale = TRUE) %>% summary()
-prcomp(cul_data, center = TRUE, scale = TRUE) %>% summary()
 
 princomp(case_data, scores = TRUE) %>% loadings()
-princomp(cul_data, scores = TRUE) %>% loadings()
 
 pca_case <- FactoMineR::PCA(case_data, graph = FALSE)
-pca_cul <- FactoMineR::PCA(cul_data, graph = FALSE)
 
 # Giá trị riêng và phần trăm phương sai giải thích
 left <- pca_case %>% factoextra::fviz_eig(.,
@@ -97,7 +72,6 @@ right <- pca_case %>% factoextra::fviz_screeplot(.,
                                                  addlabels = TRUE,
                                                  repel = TRUE)
 gridExtra::grid.arrange(left, right, ncol = 2)
-
 
 # Biplot
 pca_case %>% factoextra::fviz_pca_biplot(.,
@@ -128,26 +102,16 @@ summary(pca_case)
 # Tương quan cos2 với Dim
 var <- pca_case %>% factoextra::get_pca_var()
                     corrplot(var$cos2, is.corr = FALSE)
-var <- pca_cul %>% factoextra::get_pca_var()
-                    corrplot(var$cos2, is.corr = FALSE)
 
 # Kiểm định Barlett - KMO
 case_data %>% psych::cortest.bartlett()
 case_data %>% psych::KMO()
-
-cul_data %>% psych::cortest.bartlett()
-cul_data %>% psych::KMO()
-
 
 # Phân tích nhân tố
 Nfacs <- 3
 fa_case <- case_data %>% select(-Long.An) %>%
          factanal(.,
                   Nfacs,
-                  rotation = "varimax")
-fa_cul <- cul_data %>% 
-         factanal(.,
-                  2,
                   rotation = "varimax")
 
 # Ma trận xoay
@@ -160,11 +124,17 @@ principal(case_data %>% select(-Long.An),
 
 # Biểu đồ nhân tố
 fa.diagram(fa_case$loadings)
-fa.diagram(fa_cul$loadings)
 
-# Nhân tố với Factor Loadings = 5555
+# Nhân tố với Factor Loadings = 0.55
 fa_case %>% factor.plot(., cut = 0.55)
-fa_cul %>% factor.plot(., cut = 0.55)
 
+# vass và omega
+case_data %>% psych::vss()
+case_data %>% psych::omega(pca_data)
 
-
+# k-means với k = 4
+km_case <- case_data %>% kmeans(, centers = 4)
+fviz_cluster(object = km_case, 
+             data = case_data, 
+             labelsize = 0) + 
+  theme_minimal()
